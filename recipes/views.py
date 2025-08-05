cf29e6d3614e85c6d4f736a9e00fec6d38ea8af9
Victor <vhcaccomp@gmail.com>
2025-08-05 17:26:14 -0300

    Removendo contact e about

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/home.html')
 
-def about(request):
-    return HttpResponse('Vamos Flamengo!')
 
-def contact(request):
-    return HttpResponse('Contact')
